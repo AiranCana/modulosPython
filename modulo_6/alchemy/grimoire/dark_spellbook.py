4cf8bf55909a2c5a25706f6aaa4ae4ad74de6c95
AiranCana <airancc2005@gmail.com>
2026-04-13 15:56:52 +0200

    all good

--- modulo_6/alchemy/grimoire/dark_spellbook.py
+++ modulo_6/alchemy/grimoire/dark_spellbook.py
@@ -1,1 +1,11 @@
-    
+from .dark_validator import validate_ingredients
+
+
+def dark_spell_allowed_ingredients() -> list[str]:
+    return ["earth", "air", "fire", "water"]
+
+
+def dark_spell_record(spell_name: str, ingredients: str) -> str:
+    val = validate_ingredients(ingredients)
+    c = f"Spell recorded {spell_name} ({ingredients} - {val})"
+    return c
--- modulo_6/alchemy/grimoire/dark_validator.py
+++ modulo_6/alchemy/grimoire/dark_validator.py
@@ -1,1 +1,9 @@
-    
+from .dark_spellbook import dark_spell_allowed_ingredients
+
+
+def validate_ingredients(ingredients: str) -> str:
+    lis = dark_spell_allowed_ingredients()
+    for element in lis:
+        if element in ingredients:
+            return "VALID"
+    return "INVALID"
